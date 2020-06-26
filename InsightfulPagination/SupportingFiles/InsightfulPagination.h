34f6da33fcbd3d38220aa52759872bdaa668918c
William Boles <wibosco@users.noreply.github.com>
2020-06-26 13:20:56 +0100

    Initial project structure

---
+++ InsightfulPagination/SupportingFiles/InsightfulPagination.h
@@ -0,0 +1,19 @@
+//
+//  InsightfulPagination.h
+//  InsightfulPagination
+//
+//  Created by William Boles on 26/06/2020.
+//  Copyright © 2020 William Boles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InsightfulPagination.
+FOUNDATION_EXPORT double InsightfulPaginationVersionNumber;
+
+//! Project version string for InsightfulPagination.
+FOUNDATION_EXPORT const unsigned char InsightfulPaginationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InsightfulPagination/PublicHeader.h>
+
+
